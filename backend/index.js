182076b56fe6e74687f167a0dc37ca78851a69dd
Satendra Kumar Parteti <satendrakumarparteti.work@gmail.com>
2024-12-20 00:23:21 +0530

    cors errorrrrrrrr

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,13 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Enable CORS
-app.use(cors());
+const allowedOrigins = ['https://short-n-share-by-satendra.vercel.app'];  // frontend URL
+app.use(cors({
+  origin: allowedOrigins,  // Only allow requests from this origin
+  methods: ['GET', 'POST'],  // Allowed methods
+  allowedHeaders: ['Content-Type'],  // Allowed headers
+}));
+// app.use(cors());
 
 // Connect to MongoDB
 connectMongoDB(process.env.MONGO_URI);
